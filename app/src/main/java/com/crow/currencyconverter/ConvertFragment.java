0e5658d18f783bac770b13536cf910433ffefd74
kraxarn <emil202821@hotmail.com>
2018-11-19 13:56:07 +0100

    Fix spinner

--- app/src/main/java/com/crow/currencyconverter/ConvertFragment.java
+++ app/src/main/java/com/crow/currencyconverter/ConvertFragment.java
@@ -6,40 +6,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class ConvertFragment extends Fragment
 {
-	private String[] currencies;
-
-	public ConvertFragment()
-	{
-		currencies = new String[]
-		{
-			"ITEM_0",
-			"ITEM_1",
-			"ITEM_2"
-		};
-
-		/*
-		Spinner spinner = (Spinner) findViewById(R.id.spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
-ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-        R.array.planets_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-spinner.setAdapter(adapter);
-		 */
-	}
-
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
 		// Inflate the layout for this fragment
 		View view = inflater.inflate(R.layout.fragment_convert, container, false);
 
+		// Simple error checking and to get rid of warnings
+		if (getContext() == null)
+			return view;
+
+		// Get spinner
 		Spinner spinnerCurrencies = view.findViewById(R.id.spinner_currencies);
+
+		// Create array adapter from out strings
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.currencies, android.R.layout.simple_spinner_item);
+
+		// Specify layout
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		// Apply adapter
+		spinnerCurrencies.setAdapter(adapter);
 
 
 		return view;
